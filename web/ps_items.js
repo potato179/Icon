59e2b9d2fa25eb178d3395cdcd4957af325c0da5
홍은표 <44293278+potato179@users.noreply.github.com>
2022-09-22 22:20:09 +0900

    Update ps_items.js

--- web/ps_items.js
+++ web/ps_items.js
@@ -1,7 +1,7 @@
 const mysql_con = require("./public/js/mysql_con.js");
 var con = mysql_con.con;
 
-function writeitem_html(res, req, next){
+function writeitem_html(req, res, next){
     res.sendFile("writeitem.html", {root: __dirname});
 }
 
