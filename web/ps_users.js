6a9d963360a595740313d951256b7bfce0938317
홍은표 <44293278+potato179@users.noreply.github.com>
2022-09-22 21:42:34 +0900

    앗 복북..ㅎㅎ

--- web/ps_users.js
+++ web/ps_users.js
@@ -1,149 +1,82 @@
-const mysql_con = require('./public/js/mysql_con.js');
+const mysql_con = require("./public/js/mysql_con.js");
 var con = mysql_con.con;
 
-function get_itemss(req, res, next){
-    var q = `select * from items`;
-    con.query(q, function(err, result){
-        if(err) throw err;
-        res.send(result);
-    });
+function join_html(req, res, next){
+    res.sendFile('join.html', {root: __dirname});
 }
 
-function view_items(req, res, next){
-    console.log(req.query.id);
-    var q = `select * from items where id = "${req.query.id}";`
-    con.query(q, function(err, result){
-        if(err) throw err;
+function login_html(req, res, next){
+    res.sendFile('login.html', {root: __dirname});
+}
+
+function login(req, res, next){
+    var email = req.query.email;
+    var pw = req.query.pw;
+    var q = `select * from users where email = "${email}"`;
+    con.query(q, function (err, result) {
+        if (err) throw err;
+
         if(result[0] === undefined){
-            res.send(`존재하지 않는 질문입니다.`);
-            return;
+            res.send({
+                condition: "fail",
+                message: "존재하지 않은 유저입니다."
+            });
         }
-        res.send(result[0]);
-    })
-}
+        else{
+            if(result[0].password === pw){
+                res.cookie("userEmail", email);
+                res.cookie("username", result[0].name);
 
-function modify_items(req, res, next){
-    var c = req.query.context;
-    var id = req.query.id;
-    var q = `update items set context = "${c} where id = "${id}";`;
-    con.query(q, function (err, result) {
-        if(err) throw err;
-        console.log(result);
-        res.send({
-            message: `수정이 완료되었습니다.`
-        });
-    }); 
-}
-
-function writeitems(req, res, next){
-    var writer = req.query.writer;
-    var title = req.query.title;
-    var context = req.query.context;
-    var q = `insert into items (writer, title, context, state, comments) values ("${writer}", "${title}", "${context}", "대기", json_object())`;
-    con.query(q, function (err, result) {
-        if(err) throw err;
-        console.log(result);
-        res.send({
-            condition: "register",
-            message: "등록이 완료되었습니다."
-        });
+                res.send({
+                    condition: "success",
+                    message: "로그인되었습니다."
+                });
+            }
+            else{
+                res.send({
+                    condition: "fail",
+                    message: "비밀번호가 틀렸습니다."
+                });
+            }
+        }
     });
 }
 
-function del(req, res, next){
-    var id = req.query.id;
-    var q = util.format(`delete from items where id = "${id}";`);
-    con.query(q, function(err, result){
-        if(err) throw err;
-        console.log(result)
-        res.send({
-            condition: "delete",
-            message: "삭제되었습니다."
-        });
-    });
+function logout(req, res, next){
+    res.cookie("userEmail", "");
+    res.sendFile('index.html', {root: __dirname});
 }
 
-function writeitems_html(req, res, next){
-    res.sendFile('writeitems.html', {root: __dirname});
-}
-
-function write_comment(req, res, next){
-    var comment = req.query.comments;
-    var id = req.query.id;
+function join(req, res, next){
+    var name = req.query.name;
     var email = req.query.email;
-    var userName = req.query.userName;
+    var pw = req.query.pw;
+    var q = `select * from users where email = "${email}"`;
+    con.query(q, function (err, result) {
+        if (err) throw err;
 
-    var q = `select comments from items where id = ${id}`
-    con.query(q, function(err, result){
-        if(err) throw err;
-        console.log(result[0].comments);
-        var jsonData = JSON.parse(result[0].comments);
-        jsonData.push({name: userName, email: email, text: comment});
-
-        var jsonString = "";
-        jsonString += "json_array(";
-
-        var count = jsonData.length;
-        jsonData.forEach(function(element, index){
-            jsonString += `json_object("name", "${element.nname}, "text", "${element.text}", "email", "${element.email}")`;
-            if(index < count-1) jsonString += ",";
-        });
-    
-        jsonString += ")";
-        console.log("asdfadsf", jsonString)
-        var q = `update items set comments = ${jsonString} where id = "${id}`;
-        con.query(q, function(err, result){
-            if(err) throw err;
-            console.log(result)            
+        if(result[0] === undefined){
+            var q = `insert into users (name, email, password) values ("${name}", "${email}, "${pw}");`
+            con.query(q, function (err, result) {
+                if(err) throw err;
+                console.log(result);
+                res.send({
+                    condition: "join",
+                    message: "회원가입이 완료되었습니다."
+                });
+            }); 
+        }
+        else{
             res.send({
-                condition: "comment",
-                message: "댓글이 등록되었습니다.."
+                condition: "fail",
+                message: "이미 있는 유저입니다. 다시하세요"
             });
-        });
+        }
     });
 }
 
-function press_like(req, res, next){
-    var email = req.query.email;
-    var id = req.query.id;
-    console.log(id);
-    var s = `select likes from items where id = ${id};`;
-    con.query(s, function(err, result){
-        if(err) throw err;
-
-        var email_list = [];
-        if(result[0] != null && result[0].likes != null){
-            email_list = JSON.parse(result[0].likes);            
-        } 
-
-        if(email_list.includes(email)) return res.send({message: "이미 눌렀습니다."});
-        email_list.push(email);
-
-        var s = `update items set likes = json_array(`;
-        var count = email_list.length;
-
-        email_list.forEach(function(element, index){
-            s += `"${element}`;
-            if(index < count - 1) s += ",";
-        });
-        s += `) where id = ${id};`;
-        console.log(s);
-
-        con.query(s, function(err, result){
-            if(err) throw err;
-            res.send({
-                condition: "liked",
-                message: "좋아요를 눌렀습니다."
-            });
-        });
-    });
-}
-
-exports.get_itemss = get_itemss;
-exports.view_items = view_items;
-exports.modify_items = modify_items;
-exports.writeitems_html = writeitems_html;
-exports.del = del;
-exports.writeitems = writeitems;
-exports.write_comment = write_comment;
-exports.press_like = press_like;
+exports.login_html = login_html;
+exports.join_html = join_html;
+exports.login = login;
+exports.logout = logout;
+exports.join = join;
