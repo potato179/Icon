46cd0d9a0b36d558a6c1c40499f41ed29bdb796d
홍은표 <44293278+potato179@users.noreply.github.com>
2022-09-23 16:56:23 +0900

    Update ps_items.js

--- web/ps_items.js
+++ web/ps_items.js
@@ -5,13 +5,14 @@ function writeitem_html(req, res, next){
     res.sendFile("writeitem.html", {root: __dirname});
 }
 
-function ideas_html(req, res, next){
-    res.sendFile("ideas.html", {root: __dirname});
-}
-
 function writeitem(req, res, next){
     
 }
 
+function ideas_html(req, res, next){
+    res.sendFile("ideas.html", {root: __dirname});
+}
+
 exports.writeitem_html = writeitem_html;
+exports.writeitem = writeitem;
 exports.ideas_html = ideas_html;
